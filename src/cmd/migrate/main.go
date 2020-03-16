34daccff7f93e7c950c55625fcd2f9ecd6a636dd
Ilja Tollu <im.tollu@gmail.com>
2020-03-16 02:44:21 +0200

    add CLI util to migrate DB

---
+++ src/cmd/migrate/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"log"
+	"strings"
+)
+
+func main() {
+	var (
+		dbUrl string
+		clean bool
+	)
+	flag.StringVar(&dbUrl, "db", "", "URL-encoded database connection string (required)")
+	flag.BoolVar(&clean, "clean", false, "Drop the database and recreate from scratch")
+	flag.Parse()
+	if dbUrl == "" {
+		log.Fatal("Parameter 'db' is required!")
+	}
+	m := getMigration(dbUrl)
+	defer closeMigrate(m)
+	if clean {
+		cleanDb(m)
+	}
+	migrateDb(m)
+}
+
+func cleanDb(m *migrate.Migrate) {
+	log.Println("Cleaning database...")
+	err := m.Drop()
+	if err != nil {
+		log.Fatalf("Could not clean database: %v", err)
+	}
+	log.Println("Database clean.")
+}
+
+func migrateDb(m *migrate.Migrate) {
+	log.Println("Migrating database...")
+	err := m.Up()
+	if err != nil {
+		log.Fatalf("Could not migrate the database up: %v", err)
+	}
+	log.Println("Database migrated.")
+}
+
+type logger struct {
+}
+
+func (l logger) Printf(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
+func (l logger) Verbose() bool {
+	return false
+}
+
+func getMigration(dbUrl string) *migrate.Migrate {
+	m, readMigrationErr := migrate.New("file://migrations", dbUrl)
+	if readMigrationErr != nil {
+		log.Fatalf("could not instantiate migration: %v", readMigrationErr)
+	}
+	m.Log = logger{}
+	return m
+}
+
+func closeMigrate(m *migrate.Migrate) {
+	srcErr, dbErr := m.Close()
+	errs := make([]string, 0)
+	if srcErr != nil {
+		errs = append(errs, srcErr.Error())
+	}
+	if dbErr != nil {
+		errs = append(errs, dbErr.Error())
+	}
+	if len(errs) > 0 {
+		log.Fatalf("Could not finish Migrate: \n%s", strings.Join(errs, "\n"))
+	}
+}
--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,8 @@ module github.com/im-tollu/event-store-go
 
 go 1.13
 
-require github.com/lib/pq v1.3.0
+require (
+	github.com/golang-migrate/migrate/v4 v4.2.0
+	github.com/google/uuid v1.1.1
+	github.com/lib/pq v1.3.0
+)
