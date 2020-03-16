38d5c6eac5b4f9c8d205aec981c0049ed4860541
Ilja Tollu <im.tollu@gmail.com>
2020-03-16 22:39:12 +0200

    add create & retrieve Stream

--- src/estore/estore.go
+++ src/estore/estore.go
@@ -6,10 +6,17 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const DbUrlEnvVar = "ESTORE_DB_URL"
-
 type Repo struct {
 	db *sql.DB
+}
+
+type StreamDef struct {
+	Key string
+}
+
+type Stream struct {
+	Key     string
+	Version uint
 }
 
 func NewRepo(url string) *Repo {
@@ -22,4 +29,32 @@ func NewRepo(url string) *Repo {
 		panic(fmt.Errorf("db ping failed, url[%s]:\n%w", url, pingErr))
 	}
 	return &Repo{db: db}
+}
+
+func (r *Repo) CreateStream(def StreamDef) (Stream, error) {
+	stream := Stream{Key: def.Key}
+	tx, txErr := r.db.Begin()
+	if txErr != nil {
+		return Stream{}, fmt.Errorf("cannot start DB transaction: %w", txErr)
+	}
+	row := tx.QueryRow("insert into STREAMS (KEY, VERSION) values ($1, 1) returning VERSION", def.Key)
+	insertErr := row.Scan(&stream.Version)
+	if insertErr != nil {
+		return stream, fmt.Errorf("cannot insert stream %v into DB: %w", def, insertErr)
+	}
+	commitErr := tx.Commit()
+	if commitErr != nil {
+		return stream, fmt.Errorf("cannot commit transaction after inserting stream %v into DB: %w", def, commitErr)
+	}
+	return stream, nil
+}
+
+func (r *Repo) RetrieveStream(key string) (Stream, error) {
+	stream := Stream{}
+	row := r.db.QueryRow("select s.key, s.version from STREAMS s where KEY = $1", key)
+	selectErr := row.Scan(&stream.Key, &stream.Version)
+	if selectErr != nil {
+		return stream, fmt.Errorf("cannot retrieve stream [%v] from DB: %w", key, selectErr)
+	}
+	return stream, nil
 }
--- src/estore/estore_test.go
+++ src/estore/estore_test.go
@@ -1,6 +1,9 @@
 package estore
 
 import (
+	"flag"
+	"github.com/google/uuid"
+	"log"
 	"os"
 	"testing"
 )
@@ -14,9 +17,39 @@ func TestMain(m *testing.M) {
 }
 
 func setupRepo() {
-	dbUrl := os.Getenv(DbUrlEnvVar)
+	var dbUrl string
+	flag.StringVar(&dbUrl, "db", "", "URL-encoded database connection string (required)")
+	flag.Parse()
 	if dbUrl == "" {
-		panic(DbUrlEnvVar + " env var must be set!")
+		log.Fatal("Parameter 'db' is required!")
 	}
+	log.Printf("Running tests with database [%s]", dbUrl)
 	repo = NewRepo(dbUrl)
+}
+
+func TestCreateStream(t *testing.T) {
+	streamKey := uuid.New().String()
+	def := StreamDef{
+		Key: streamKey,
+	}
+	newStream, createErr := repo.CreateStream(def)
+	if createErr != nil {
+		t.Fatalf("cannot create stream because of error: %v", createErr)
+	}
+	if newStream.Key != def.Key {
+		t.Errorf("expected that created stream has Key [%s], but got [%s]", def.Key, newStream.Key)
+	}
+	if newStream.Version != 1 {
+		t.Errorf("expected that created stream has version [1], but got [%d]", newStream.Version)
+	}
+	stream, retrieveErr := repo.RetrieveStream(streamKey)
+	if retrieveErr != nil {
+		t.Fatalf("cannot retrieve stream because of error: %v", retrieveErr)
+	}
+	if stream.Key != def.Key {
+		t.Errorf("expected that retrieved stream has Key [%s], but got [%s]", def.Key, stream.Key)
+	}
+	if stream.Version != 1 {
+		t.Errorf("expected that retrieved stream has version [1], but got [%d]", stream.Version)
+	}
 }
